237ac3b2ecf9844bca8e9e829d810232f9c161ae
daxin <ekescheng@qq.com>
2019-11-11 02:35:28 -0500

    update install.yml

--- files/rabbitinit.sh
+++ files/rabbitinit.sh
@@ -1,5 +1,5 @@
 rabbitmq-server -detached
-sleep 3
+sleep 1
 rabbitmq-plugins enable rabbitmq_management
 rabbitmqctl add_vhost saicgormq
 rabbitmqctl add_user  admin Admin12#45
@@ -15,7 +15,8 @@ rabbitmqctl delete_user guest
 rm -rf /var/lib/rabbitmq/.erlang.cookie
 echo "vxZhJUa95q1+W5xTuJO/xA==" > /var/lib/rabbitmq/.erlang.cookie
 chown -R rabbitmq:rabbitmq /var/lib/rabbitmq
+chmod 600 /var/lib/rabbitmq/.erlang.cookie
 sleep 1
 rabbitmqctl stop
-sleep 3
+sleep 1
 rabbitmq-server -detached
